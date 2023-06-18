802f79e226a3554dce4f1336a949babaa0a1595d
ltuckr <l.tucker.rdh@gmail.com>
2023-06-18 18:11:57 -0400

    finished question portion of index

--- Develop/index.js
+++ Develop/index.js
@@ -2,66 +2,14 @@
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questionArray = [
 
         {
             type: 'input',
-            name: 'name',
-            message: 'Welcome to your professional README generator! Please, provide your preferred legal name as you would like for it to appear on your README:',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please provide your first and last name');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Please enter your GitHub username:',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username');
-                    return false;
-                }
-            }
-        }, 
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Please enter an email address:',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter an email address');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'portfolio',
-            message: 'Please enter your portfolio URL or linkedIn:',
-            validate: portfolioInput => {
-                if (portfolioInputnput) {
-                    return true;
-                } else {
-                    console.log('Please enter additional contact URL');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
             name: 'title',
-            message: 'What is the name of your project?',
-            validate: titleInput => {
-                if (titleInput) {
+            message: 'What is the title of your project?',
+            validate: title=> {
+                if (title) {
                     return true;
                 } else {
                     console.log('Please enter the name of your project to proceed with your README.');
@@ -72,7 +20,7 @@ const questions = [
         {
             type: 'input',
             name: 'description',
-            message: "Please write a brief description of your project here",
+            message: "Please write a brief description of your project here.",
             validate: descriptionInput => {
                 if (descriptionInput) {
                     return true;
@@ -82,6 +30,99 @@ const questions = [
                 }
             }
         },
+        {
+            type: 'input',
+            name: 'table',
+            message: 'Please provide your table of contents.',
+            validate: installationTable => {
+                if (installationTable) {
+                    return true;
+                } else {
+                    console.log('Please provide your table of contents.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please provide installation instructions.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information for your project.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide usage information.');
+                    return false;
+                }
+            }
+        },
+       {
+            type: 'list',
+            name: 'licensing',
+            message: 'What license would you like to include?',
+            selections: ['gpl','artistic-2.0','wtfpl','cc','ms-pl','mit','unlicense'],
+            when: ({ licensingInput }) => {
+                if (licensingInput) {
+                    return true;
+                } else {
+                    console.log('Please select a license for your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please provide links to any contributing repos, websites or tutorials that you used when creating this project.',
+            validate: contributingInput => {
+                if (contributingInput) {
+                    return true;
+                } else {
+                    console.log('Please provide links to any materials used that helped you create your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide tests for your application, and explain how they run.',
+            validate: testsInput => {
+                if (testsInput) {
+                    return true;
+                } else {
+                    console.log('Please provide tests for your application, and explain how they run.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please provide your Github username and your email address.',
+            validate: questionsInput => {
+                if (questionsInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your Github username and your email address.'');
+                    return false;
+                }
+            }
+        },
+
 ];
 
 // TODO: Create a function to write README file
