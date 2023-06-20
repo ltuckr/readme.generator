ff7f60d8264c209635417f19299dd0d62d4344fd
ltuckr <l.tucker.rdh@gmail.com>
2023-06-20 19:39:25 -0400

    added function for returning license link and empty string

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,20 +2,46 @@ const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if (!license) {
+  
+const Badge = {
+    MPL: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    WTFPL: "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)",
+    None: "![License](https://img.shields.io/badge/License-None-lightgrey.svg)",
+  };
 
-    [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    [![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
-  }
+  } };
+
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MPL') {
+    return `https://choosealicense.com/licenses/mpl-2.0/`
+  }
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+
+  if (license === 'WTFPL'){
+    return `http://www.wtfpl.net/` 
+  }
+
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses This project is licensed under ${license}.'
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
