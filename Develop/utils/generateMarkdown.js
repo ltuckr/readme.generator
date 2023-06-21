e6115d8175b5ffe6c67dea370169395a287bbfe3
ltuckr <l.tucker.rdh@gmail.com>
2023-06-20 20:46:30 -0400

    cleaning up syntax errors and troubleshooting

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,54 +1,49 @@
 const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license) {
-    return ``;
-
+    return '';
   } else {
-
-    if (license === 'MPL') { 
-      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    if (license === 'MPL') {
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
     }
     if (license === 'MIT') {
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
     }
-    if (license === 'WTFPL'){
-      '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)'
+    if (license === 'WTFPL') {
+      return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)';
     }
-
-  } };
-
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return ``;
-
-} else {
-
-  if (license === 'MPL') {
-    return `https://choosealicense.com/licenses/mpl-2.0/`
+  if (!license) {
+    return '';
+  } else {
+    if (license === 'MPL') {
+      return 'https://choosealicense.com/licenses/mpl-2.0/';
+    }
+    if (license === 'MIT') {
+      return 'https://lbesson.mit-license.org/';
+    }
+    if (license === 'WTFPL') {
+      return 'http://www.wtfpl.net/';
+    }
   }
-  if (license === 'MIT') {
-    return `https://lbesson.mit-license.org/`
-  }
-  if (license === 'WTFPL'){
-    return `http://www.wtfpl.net/` 
-  }
-
-
-
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) {
-    return ``;
+    return '';
   } else {
     return `## Licenses
-    This project is licensed under ${license} .`
+This project is licensed under ${license}.`;
   }
 }
 
@@ -56,41 +51,42 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ${renderLicenseBadge(data.licenses)}
+${renderLicenseBadge(data.licenses)}
 
-  ## Table of Contents
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Licenses](#licenses)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licenses](#licenses)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
-  ## Description
-  ${data.description}
 
-  ## Installation
-  ${data.installation}
+## Description
+${data.description}
 
-  ## Usage
-  ${data.usage}
+## Installation
+${data.installation}
 
-  ${renderLicenseSection(data.licenses)}
+## Usage
+${data.usage}
 
-  ## Contributing
-  ${data.contributing}
+${renderLicenseSection(data.licenses)}
 
-  ## Tests
-  ${data.tests}
+## Contributing
+${data.contributing}
 
-  ## Questions
-  Have questions about this project?  
-  GitHub: https://github.com/${data.github}  
-  Email: ${data.email}
+## Tests
+${data.tests}
+
+## Questions
+Have questions about this project?  
+GitHub: https://github.com/${data.github}  
+Email: ${data.email}
 
 
 `;
 }
+
 module.exports = generateMarkdown;
